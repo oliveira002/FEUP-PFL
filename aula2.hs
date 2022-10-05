14c5e1a649aa9bb4b4d1498cadd964e88aa15d83
oliveira <deadzin112@gmail.com>
2022-10-05 12:59:41 +0100

    f2 till 2.10

--- aula2.hs
+++ aula2.hs
@@ -1,83 +1,98 @@
--- 2.1a
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Use foldr" #-}
-
+--2.1a
 myand :: [Bool] -> Bool
 myand [] = True
-myand (x:xs) = x && myand xs 
+myand(x:xs) = x && myand xs
 
--- 2.1b 
-
+--2.1b
 myor :: [Bool] -> Bool
 myor [] = False
-myor (x:xs) = x || myor xs
+myor(x:xs) = x && myor xs
 
--- 2.1c
-
+--2.1c
 myconcat :: [[a]] -> [a]
 myconcat [] = []
-myconcat (x:xs) = x ++ myconcat xs
+myconcat(x:xs) = x ++ myconcat xs
 
-
--- 2.1d
+--2.1d
 myreplicate :: Int -> a -> [a]
 myreplicate 0 a = []
-myreplicate n x = [x] ++ myreplicate(n-1) x
+myreplicate b a = [a] ++ myreplicate (b-1) a
 
+--2.1e
+myidx :: [a] -> Int -> a
+myidx (x:xs) 0 = x
+myidx (x:xs) n = myidx(xs) (n-1)
 
--- 2.1e
-mye :: [a] -> Int -> a
-mye [] a = error "erro"
-mye(x:xs) 0 = x
-mye(x:xs) a = mye(xs) (a-1)
+--2.1f
+myele :: Eq a => a -> [a] -> Bool
+myele a [] = False
+myele e (x:xs) = (x == e) || myele e (xs) 
 
+--2.2
+myint :: a -> [a] -> [a]
+myint a [] = []
+myint a (x:xs) | (not (null xs)) = [x] ++ [a] ++ myint a xs
+               | otherwise = [x]
 
--- 2.1f
-myelem :: Eq a => a -> [a] -> Bool
-myelem a [] = False
-myelem a (x:xs) = a == x || myelem a (xs) 
+--2.3
+mdc :: Integer -> Integer -> Integer
+mdc a b =
+    if (b == 0) then a
+    else mdc b (mod a b)
 
--- 2.2
-myinter :: a -> [a] -> [a]
-myinter a [] = []
-myinter a (x:xs) | length xs == 0 = [x]
-                 | otherwise = [x] ++ [a] ++ myinter a xs
+--2.4a
+insert :: Ord a => a -> [a] -> [a]
+insert x [] = [x]
+insert n (x:xs) | n < x = [n] ++ [x] ++ xs
+                | otherwise = [x] ++ insert n xs
 
-
--- 2.3
-mdc :: Int -> Int -> Int
-mdc a b | b == 0 = a
-        | otherwise = mdc b (a `mod` b)
-
--- 2.4a
-myins :: Ord a => a -> [a] -> [a]
-myins a [] = [a]
-myins a (x:xs) | a < x = a : x : xs
-               | otherwise = x: myins a (xs)
-
-
-myisort :: Ord a => [a] -> [a]
-myisort [] = []
-myisort (x:xs) = myins x (myisort xs)
-
+--2.4b
+isort :: Ord a => [a] -> [a]
+isort [] = []
+isort (x:xs) = insert x (isort xs)
 
 --2.5a
-minimumA :: Ord a => [a] -> a
-minimumA [x] = x
-minimumA (x:xs) =
-        if (x < minimumA xs) then x
-        else minimumA xs
+mini :: Ord a => [a] -> a
+mini [x] = x
+mini (x:xs) = if (x < mini xs) then x
+              else mini xs
 
 --2.5b
-delete :: Eq a => a -> [a] -> [a]
-delete _ [] = []
-delete a (x:xs) | a == x = xs
-                | otherwise = x : delete a xs
+del :: Eq a => a -> [a] -> [a]
+del n [] = []
+del n (x:xs) | x == n = xs
+             | otherwise = x : del n xs
+
+--2.5c
+ssort :: Ord a => [a] -> [a] 
+ssort [] = []
+ssort a = mini a : ssort (del (mini a) a)
 
 --2.6
-mysum :: [Int] -> Int
-mysum a = sum(map (^2) a)
+-- sum([x^2 | x<-[1..100]])
+-- sum(map (^2) [1..100])
+
 
 --2.7a
-aprox :: Int -> Double
-aprox n = sum [(-1)^k / fromIntegral (2*k +1) | k<-[0..n]]
+aproxi :: Int -> Double
+aproxi n = 4 * sum([(-1)^k /  fromIntegral (2*k + 1) | k<-[0..n]]) -- why fromIntegral?
+
+--2.7b
+aproxii :: Int -> Double
+aproxii n = sqrt(12 * sum( [(-1)^k / fromIntegral(k + 1)^2 | k<-[0..n]]))
+
+--2.8
+dotprod :: [Float] -> [Float] -> Float
+dotprod a b = sum[x * y | (x,y) <- zip a b]
+--dotprod [] [] = 0
+--dotprod (x:xs) (y:ys) = x * y + dotprod xs ys
+
+--2.9
+divprop :: Integer -> [Integer]
+divprop n = [x | x<-[1..n-1], n `mod` x == 0]
+--divprop n = filter (\x -> n `mod` x == 0) [1..n-1]
+
+--2.10
+perfeitos :: Integer -> [Integer]
+--perfeitos n = [x | x<-[1..n], (sum (divprop x) == x)]
+perfeitos n = filter (\x -> sum(divprop x) == x) [1..n]
